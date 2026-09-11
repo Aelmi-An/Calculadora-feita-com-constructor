c65f6e104319a3105b90920321935a458e8335b6
Felipe Almeida <felpson654@gmail.com>
2026-09-11 09:54:39 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,17 +4,6 @@
            console.log(Operação)
            }
 
-           class Dígitos{
-        #N1m
-        #N2m
-        #Func
-
-            constructor(parameters) {
-        this.#N1m=N1m
-        this.#N2m=N2m
-        this.#Func=Func
-            }
-           }
         class Coleta{
         #Input_N1
         #Input_N2
@@ -27,30 +16,30 @@
             this.#Btn_Calcular=document.getElementByID("btnCalcular")
             this.#divResultado=document.getElementByID("resultado")
         }
+
         #obterDadosFormulario() {
-        const nome = this.#inputNome.value.trim();
-        const sobrenome = this.#inputSobrenome.value.trim();
-        const anoNascimento = parseInt(this.#inputAnoNascimento.value);
-
-        if (!nome || !sobrenome || isNaN(anoNascimento)) {
-            throw new Error("Por favor, preencha todos os campos corretamente.");
+        const N1m = this.#inputNome.value.trim();
+        const N2m = this.#inputSobrenome.value.trim();
+        const Resposta = this.#divResultado
+        if (!N1m || !N2m) {
+            throw new Error("Insira os números corretamente");
         }
-
-        return { nome, sobrenome, anoNascimento };
+      }
     }
-    }
-        calcularIdade() {
+        Calculações() {
        if(Operação="-"){
-
+         let Resultado = N1m-N2m;
        };
        else if(Operação="+"){
-
+         let Resultado = N1m-N2m;
        };
        else if(Operação="/"){
-
+          if(N2m == 0){
+         Resposta.innerText
+          }
        };
        else if(Operação="*"){
-
+         let Resultado = N1m*N2m;
        };
        else{
         alert("Isso nem é possível")
